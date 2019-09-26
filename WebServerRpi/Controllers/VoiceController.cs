7e2f1442c159b54ee8a3735af9596bfd4cc071d7
seanchaanine <45476609+SeanDaDon@users.noreply.github.com>
2019-09-26 01:50:49 -0700

    Attempted to implement python IPC. Need to add logging

--- WebServerRpi/Controllers/VoiceController.cs
+++ WebServerRpi/Controllers/VoiceController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace WebServerRpi.Controllers
 {
@@ -12,6 +13,30 @@ namespace WebServerRpi.Controllers
         public IActionResult Voice()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Submit(int id, string name)
+        {
+            ViewBag.Id = id;
+            ViewBag.Name = name;
+            try
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.UseShellExecute = false;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.FileName = @"usr/bin/python";
+                startInfo.Arguments = @"home/pi/test/python motorScript.py -i yo -a 1";
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+            catch(Exception e)
+            {
+                //Implement some form of logging...
+                return View("Index");
+            }
+            return View("Voice");
         }
 
     }
