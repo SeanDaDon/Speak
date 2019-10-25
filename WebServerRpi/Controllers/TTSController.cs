d2b3139f341a981e1133ee1ca562da1fc779f6bf
seanchaanine <45476609+SeanDaDon@users.noreply.github.com>
2019-10-25 11:55:11 -0700

    Commit to save

---
+++ WebServerRpi/Controllers/TTSController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServerRpi.Controllers
+{
+    public class TTSController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
