a8343cd9d86febef305ea26c73363668a7ccc0ae
seanchaanine <45476609+SeanDaDon@users.noreply.github.com>
2019-11-21 02:14:12 -0800

    commit to save

--- WebServerRpi/Controllers/TTSController.cs
+++ WebServerRpi/Controllers/TTSController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,32 @@ namespace WebServerRpi.Controllers
     {
         public IActionResult Index()
         {
+            return View("Speaker");
+        }
+
+        [HttpGet]
+        public ActionResult Submit(int id)
+        {
+            ViewBag.Id = id;
+
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+
+            string payLoad = $" -a {id}";
+            //try
+            //{
+            //    startInfo.FileName = @"/usr/bin/python";
+            //    startInfo.Arguments = @"/home/pi/teamSpeak/motorScript.py" + payLoad;
+            //    process.StartInfo = startInfo;
+            //    process.Start();
+            //}
+            //catch (Exception e)
+            //{
+            //    //Implement some form of logging...
+            //    return View("Index");
+            //}
             return View("Speaker");
         }
     }
